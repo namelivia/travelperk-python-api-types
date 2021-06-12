c6d7035a6be55e8b8c6d7b39de95a15df5511323
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-06-12 19:37:27 +0200

    Restriction end date may be optional

--- travelperk_python_api_types/travelsafe/restrictions/restriction.py
+++ travelperk_python_api_types/travelsafe/restrictions/restriction.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from pydantic.dataclasses import dataclass
 from travelperk_python_api_types.travelsafe.location import Location
 from .requirement import Requirement
@@ -13,7 +13,7 @@ class Restriction:
     summary: str
     details: str
     start_date: str
-    end_date: str
+    end_date: Optional[str]
     updated_at: str
     requirements: List[Requirement]
     info_source: InfoSource
